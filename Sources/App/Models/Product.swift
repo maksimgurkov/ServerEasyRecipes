8f8b6a67daf2e4b99baa4e4d125e95aab04d7247
Maksim Gurkov <maksim.gurkov@gmail.com>
2024-06-18 09:30:07 +0300

    Test

--- Sources/App/Models/Product.swift
+++ Sources/App/Models/Product.swift
@@ -4,7 +4,7 @@ import struct Foundation.UUID
 /// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
 /// It is recommended you write your model with sendability checking on and then suppress the warning
 /// afterwards with `@unchecked Sendable`.
-final class Todo: Model, @unchecked Sendable {
+final class Product: Model, @unchecked Sendable {
     static let schema = "todos"
     
     @ID(key: .id)
